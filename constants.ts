7774fc7fdb232e4f6aaa07abf434fc72cd733f76
Matthew Zimmerman <s20400608@gmail.com>
2023-10-29 12:21:07 -0700

    added files

---
+++ constants.ts
@@ -0,0 +1,16 @@
+
+export const socials = [
+    {
+        id: 1,
+        name: "Github",
+        url: "https://github.com/M4ttZ1", 
+        handle:"@M4ttZ1"   
+    },
+    {
+        id: 2,
+        name: "Personal Website",
+        url: "https://e-portfolio-fv49kqajk-matthew-zimmermans-projects.vercel.app/",
+        handle: ""
+    }
+]
+
